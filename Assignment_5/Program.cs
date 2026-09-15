5c5021a9e6a4a567a24c19951d26b087eb7958b6
Menna Saber <mennasaper@gmail.com>
2026-09-15 15:55:27 +0300

    Question4

--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Assignment_5
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Assignment_5
 {
     internal class Program
     {
+
+        enum Genre { 
+            Fiction, 
+            NonFiction, 
+            Science 
+        }
+
+
+        class Book
+        {
+         public Genre Genre { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            
+
+            // **********     Part 2 from assignment 5     **********
+
+            #region Question4
+            ////4-Declare an enum Genre { Fiction, NonFiction, Science }. Add a Genre
+            ////property to Book, assign it Genre.Science, and print it.
+
+            //Book book = new Book();
+            //book.Genre = Genre.Science;
+            //Console.WriteLine(book.Genre);
+
+            #endregion
+
+
         }
     }
 }
